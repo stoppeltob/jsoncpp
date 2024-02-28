09563d9ef5a556166c0b3fc40c24f42a5e9b36a1
DavidPrinz1123 <161600179+DavidPrinz1123@users.noreply.github.com>
2024-02-28 19:19:01 +0000

    Letzter Test

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 namespace fs = std::filesystem;
 
 #include <jsoncpp/json/json.h>
+// Letzter Test
 // Hilfetext (David Prinz)
 const char * const HilfeText = {
     "Usage: options [OPTIONS] file ... \n" \
