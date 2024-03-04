e6f9c5db1d819dff70d1930042efc9391b79b888
stoppeltob <stoppelkamp05@gmail.com>
2024-03-04 11:03:28 +0100

    Hilfe text

--- main.cpp
+++ main.cpp
@@ -10,12 +10,15 @@ using namespace std;
 namespace fs = std::filesystem;
 
 #include <jsoncpp/json/json.h>
-// Hilfetext (David Prinz)
+// Hilfetext (David Prinz, Tobias Stoppelkamp)
 const char * const HilfeText = {
+    "Create a Windows Batch file based on the provided JSON configuration.\n" \
     "Usage: options [OPTIONS] file ... \n" \
     "-h, --help         Hier die Funktion, die beschrieben werden muessen\n" \
+    "  -o, --output=FILENAME     Set the output file name\n" \
+    "  -v, --version             Show version information\n" \
     "\n" \
-    "For more information contact david.prinz1123@gmail.com"
+    "Author team: David Prinz david.prinz1123@gmail.com, Tobias Stoppelkamp tobias.stoppelkamp05@gmail.com"
 };
 
 int main(const int argc, const char **argv) {
