63c2ebe44714d58234c4939f54326e465802cde9
Lionwicki <lionwicki@gmail.com>
2024-03-07 18:52:59 +0100

    Code for Command line arguments - not yet finished

---
+++ LW(Test folder)/main.cpp
@@ -0,0 +1,14 @@
+/*
+#include <iostream>
+#include "header.h"
+
+extern "C" {
+    #include "header.h"
+}
+
+int main(int argc, char *argv[]) {
+    processOptions(argc, argv);
+
+    return 1;
+}
+*/
