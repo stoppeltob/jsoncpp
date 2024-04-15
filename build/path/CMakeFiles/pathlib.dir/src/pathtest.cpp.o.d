efc41a1a1ed0a270bda0af851165584f57b8747e
DavidPrinz1123 <david.prinz1123@gmail.com>
2024-04-15 09:46:20 +0200

    ..

---
+++ build/path/CMakeFiles/pathlib.dir/src/pathtest.cpp.o.d
@@ -0,0 +1,4 @@
+path/CMakeFiles/pathlib.dir/src/pathtest.cpp.o: \
+ /home/david/dhbw/Programmieren1/Programmentwurf_c++/jsoncpp/path/src/pathtest.cpp \
+ /usr/include/stdc-predef.h \
+ /home/david/dhbw/Programmieren1/Programmentwurf_c++/jsoncpp/path/include/pathtest.h
