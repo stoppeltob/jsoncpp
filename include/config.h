cad5e58f133280263fad9830e1e5d4c036f166de
DavidPrinz1123 <david.prinz1123@gmail.com>
2024-04-17 10:02:20 +0200

    cmake geaendert

--- include/config.h
+++ include/config.h
@@ -2,5 +2,5 @@
 #define _CONFIG_H_
 #define MAIN_VERSION_MAJOR 1
 #define MAIN_VERSION_MINOR 0
-#define MAIN_DESCRIPTION "Eine Applikation um jsoncpp zu demonstrieren"
+#define MAIN_DESCRIPTION "Der Programmentwurf eine JSON-Datei in eine Batch zu wandeln, von Lion Wicki, David Prinz, Philipp Assfalg, Tobias Stoppelkamp."
 #endif
